28e699ced4864d4f8af95a6e53df2750e53236f1
WallfacerRZD <1048058400@qq.com>
2018-11-13 10:29:40 +0800

    变换路由时改变消息文本

--- src/app/main-page-moduel/side-nav/side-nav.component.ts
+++ src/app/main-page-moduel/side-nav/side-nav.component.ts
@@ -8,30 +8,10 @@ import {filter} from "rxjs/operators";
   styleUrls: ['./side-nav.component.css']
 })
 export class SideNavComponent implements OnInit {
-  textMap = {
-    '/message': '消息',
-    '/contact': '消息',
-    '/news': '消息',
-    '/circle': '消息',
-  };
-
-  private text: string;
-
-  getText(): string {
-    return this.text;
-  }
-
-  setText(value: string) {
-    this.text = value;
-  }
-
-  constructor(private router: Router) {
+  constructor() {
   }
 
   ngOnInit() {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(()=>this.setText(this.textMap[this.router]))
   }
 
 }
--- src/app/main-page-moduel/tool-bar/tips-message/tips-message.component.ts
+++ src/app/main-page-moduel/tool-bar/tips-message/tips-message.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'app-tips-message',
@@ -6,11 +8,33 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./tips-message.component.css']
 })
 export class TipsMessageComponent implements OnInit {
+  textMap = {
+    '/message': '消息',
+    '/contact': '联系人',
+    '/news': '看点',
+    '/circle': '动态',
+  };
 
-  constructor() {
+  text: string;
+
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => this.changeText());
   }
 
+  private getText(): string {
+    return this.text;
+  }
+
+  private setText(value: string) {
+    this.text = value;
+  }
+
+  private changeText() {
+    this.setText(this.textMap[this.router.url]);
+  }
 }
